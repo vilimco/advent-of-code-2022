c37bfa06e55994028147c5102e5918e93d9e8ee3
vilimco <vilim.stubican@gmail.com>
2022-12-14 14:26:27 +0100

    Add day 01

---
+++ day-01/part-one.py
@@ -0,0 +1,20 @@
+input_file = open('input.txt', 'r')
+input = input_file.readlines()
+
+elf_calories = 0
+elfs = []
+
+# Strips the newline character
+for rawLine in input:
+    line = rawLine.strip()
+    if line != "":
+        elf_calories += int(line)
+        continue
+
+    elfs.append(elf_calories)
+    elf_calories = 0
+
+elfs.append(elf_calories)
+elfs.sort(reverse=True)
+
+print("Max calories: ", elfs[0])
