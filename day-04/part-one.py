423a4462ccad48ce02757a39218a464e7ce7350a
vilimco <vilim.stubican@gmail.com>
2022-12-15 08:49:43 +0100

    Add day 04

---
+++ day-04/part-one.py
@@ -0,0 +1,26 @@
+input_file = open('input.txt', 'r')
+input = input_file.readlines()
+
+def split_range(line):
+    return list(map(int, line.split(sep="-")))
+
+overlaps = 0
+for rawLine in input:
+    line = rawLine.strip()
+    if line == "":
+        continue
+
+    tasks = line.split(sep=",")
+    elf1, elf2 = list(map(split_range, tasks))
+    if elf1[1] - elf1[0] < elf2[1] - elf2[0]:
+        elf1, elf2 = [elf2, elf1]
+
+    if elf1[0] <= elf2[0] and elf1[1] >= elf2[1]:
+        overlaps += 1
+
+print("Overlaps:", overlaps)
+
+
+
+
+
