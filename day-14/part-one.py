0f4b7f2fbfe1d32ece4d46c89577210e45699f59
vilimco <vilim.stubican@gmail.com>
2022-12-17 20:02:11 +0100

    Add day 14

---
+++ day-14/part-one.py
@@ -0,0 +1,58 @@
+import json
+
+input_file = open('input.txt', 'r')
+input = [value.strip() for value in input_file.readlines()]
+
+cave = {}
+abyss_start = 0
+def get_key(x, y):
+    return str(x) + "-" + str(y)
+
+
+for line in input:
+    paths = [list(map(int, value.split(","))) for value in line.split(" -> ")]
+
+    start_x, start_y = paths[0]
+
+    for i in range(1, len(paths)):
+        end_x, end_y = paths[i]
+
+        for x in range(min(start_x, end_x), max(start_x, end_x) + 1):
+            for y in range(min(start_y, end_y), max(start_y, end_y) + 1):
+                cave[get_key(x, y)] = "#"
+
+        start_x = end_x
+        start_y = end_y
+
+        abyss_start = max(abyss_start, end_y)
+
+sand_start = [500, 0]
+sand_x, sand_y = sand_start
+sands = 0
+while(True):
+    if sand_y >= abyss_start:
+        break
+
+    bottom_key = get_key(sand_x, sand_y + 1)
+    if bottom_key not in cave:
+        sand_y += 1
+        continue
+
+    left_key = get_key(sand_x - 1, sand_y + 1)
+    if left_key not in cave:
+        sand_x -= 1
+        sand_y += 1
+        continue
+
+    right_key = get_key(sand_x + 1, sand_y + 1)
+    if right_key not in cave:
+        sand_x += 1
+        sand_y += 1
+        continue
+
+    current_key = get_key(sand_x, sand_y)
+    cave[current_key] = "o"
+    sands += 1
+    sand_x, sand_y = sand_start
+
+print(sands)
